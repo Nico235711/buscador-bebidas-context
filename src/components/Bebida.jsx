a3d0fbd854a62934d06c2b7c4f5f72cea8953f2f
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-14 18:15:55 -0300

    Agregando el Componente Card a las Bebidas

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -1,8 +1,27 @@
+import { Button, Card, Col } from "react-bootstrap"
+
 
 const Bebida = ({ bebida }) => {
 
   return (
-    <div>Bebida</div>
+    <Col md={4} lg={3}>
+      <Card className="mt-5">
+        <Card.Img
+          variant="top"
+          src={bebida.strDrinkThumb}
+          alt={`Imagen de la bebida: ${bebida.strDrink}`} />
+
+          <Card.Body>
+            <Card.Title
+              style={{height: 48 }}
+              className="text-center"
+            >
+              {bebida.strDrink}
+            </Card.Title>
+            <Button className="mt-5 text-uppercase ">Ver Receta</Button>
+          </Card.Body>
+      </Card>
+    </Col>
   )
 }
 
